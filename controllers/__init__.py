68b631446bd12c060823f23c52ed257b1f991562
Edson Gabriel <edsongabrielbb@gmail.com>
2025-12-01 15:11:51 -0300

    mapa +ou- funcional

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,8 +1,10 @@
 from bottle import Bottle
 from controllers.user_controller import UserController
 from controllers.authenticator_controller import AuthenticatorController
+from controllers.point_controller import PointController
 
 def init_controllers(app: Bottle):
     
     UserController(app)
     AuthenticatorController(app)
+    PointController(app)
